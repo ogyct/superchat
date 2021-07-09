f577ce595b0972a4e6e4c7e42f319738ec210440
Dmitry Avgustis <dmitry.avgustis@zentity.com>
2021-07-09 17:38:56 +0200

    real time chat app with firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/analytics';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { useRef, useState } from 'react';
+
+if (!firebase.apps.length) {
+    firebase.initializeApp({});
+
+} else {
+    firebase.app(); // if already initialized, use that one
+}
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+function ChatMessage(props) {
+    const {
+        text,
+        uid,
+        photoURL
+    } = props.message;
+    const messageClass = uid === auth.currentUser.uid ? 'sent' : 'recieved';
+    return (<div className={ `message ${ messageClass }` }>
+        <img src={ photoURL } alt={ '' }/>
+        <p>{ text }</p>
+    </div>);
+}
+
+function ChatRoom() {
+    const dummy = useRef();
+
+    const messagesRef = firestore.collection('messages');
+    const query = messagesRef.orderBy('createdAt')
+        .limit(25);
+
+    const {
+        uid,
+        photoURL
+    } = auth.currentUser;
+
+    const [messages] = useCollectionData(query, { idField: 'id' });
+
+    const [formValue, setFormValue] = useState('');
+    const sendMessage = async (e) => {
+        e.preventDefault();
+        await messagesRef.add({
+            text: formValue,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+            uid,
+            photoURL
+        });
+        setFormValue('');
+        dummy.current.scrollIntoView({ behavior: 'smooth' });
+    };
+    return (<>
+        <main>
+            { messages && messages.map(msg => <ChatMessage key={ msg.id } message={ msg }/>) }
+            <span ref={ dummy }/>
+        </main>
+        <form onSubmit={ sendMessage }>
+            <input value={ formValue } onChange={ (e) => setFormValue(e.target.value) }/>
+            <button type="submit">🕊</button>
+        </form>
+    </>);
+}
+
+const SignIn = () => {
+    const signInWithGoogle = () => {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        auth.signInWithPopup(provider);
+    };
+
+    return <button onClick={ signInWithGoogle }>Sign in with Google</button>;
+};
+
+const SignOut = () => {
+    return auth.currentUser && (
+        <button onClick={ () => auth.signOut() }>Sign out</button>
+    );
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [user] = useAuthState(auth);
+    return (
+        <div className="App">
+            <header>
+                <h1>⚛️🔥💬</h1>
+                <SignOut/>
+            </header>
+
+            <section>
+                { user ? <ChatRoom/> : <SignIn/> }
+            </section>
+
+        </div>
+    );
 }
 
 export default App;
